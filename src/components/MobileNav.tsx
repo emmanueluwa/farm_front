5744cf7bcc7465aa72081bd66f51dab8ab9f394c
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-14 19:21:31 +0100

    adding auth functionality to mobile navbar

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -8,8 +8,12 @@ import {
 } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -17,12 +21,28 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          <span>Welcome to FarmFrsh.com</span>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-green-800" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to FarmFrsh.com</span>
+          )}
         </SheetTitle>
 
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-green-800">Login</Button>
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-green-800"
+            >
+              Login
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
