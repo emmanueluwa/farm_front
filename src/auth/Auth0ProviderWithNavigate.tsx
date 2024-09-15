fc4ca9ad6c831587c4f08c573dda282d432fcc2e
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-15 18:16:21 +0100

    added ability to create user using api call from frontend

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
@@ -5,6 +6,8 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -15,7 +18,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
 
   //called when redirected to webapp
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("USER", user);
+    //user.sub contains userid
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
