1d42b88e8051cbffa0a63a3f74aeedf81158207b
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-18 20:38:22 +0100

    adding ability to search for area, farm, and produce

--- src/api/FarmApi.tsx
+++ src/api/FarmApi.tsx
@@ -1,11 +1,17 @@
+import { SearchState } from "@/pages/SearchPage";
 import { FarmSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const userSearchFarms = (city?: string) => {
+export const userSearchFarms = (searchState: SearchState, city?: string) => {
   const createSearchRequest = async (): Promise<FarmSearchResponse> => {
-    const response = await fetch(`${API_BASE_URL}/api/farm/search/${city}`);
+    const params = new URLSearchParams();
+    params.set("searchQuery", searchState.searchQuery);
+
+    const response = await fetch(
+      `${API_BASE_URL}/api/farm/search/${city}?${params.toString()}`
+    );
 
     if (!response.ok) {
       throw new Error("failed to get farm");
@@ -15,7 +21,7 @@ export const userSearchFarms = (city?: string) => {
   };
 
   const { data: results, isLoading } = useQuery(
-    ["searchFarms"],
+    ["searchFarms", searchState],
     createSearchRequest,
     //query will not run if city undefined
     { enabled: !!city }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,14 +1,37 @@
 import { userSearchFarms } from "@/api/FarmApi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type SearchState = {
+  searchQuery: string;
+};
 
 const SearchPage = () => {
   //given the way lifecycle of react hooks works, bunch of rerendering, city could be undefined on first rerender
   const { city } = useParams();
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+  });
 
   //when city value
-  const { results, isLoading } = userSearchFarms(city);
+  const { results, isLoading } = userSearchFarms(searchState, city);
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+    }));
+  };
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -22,6 +45,12 @@ const SearchPage = () => {
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div id="produce-list">insert produce here :)</div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search Produce or Farm"
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((farm) => (
           <SearchResultCard farm={farm} />
